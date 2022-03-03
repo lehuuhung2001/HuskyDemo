66c6ca1844aa432c1f0b97b6ee1c2305e3b018ce
Le Huu Hung <huuhung1962001@gmail.com>
2022-03-03 09:28:18 +0700

    up

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TextInput} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style ={{flex: 0.4 ,alignItems: 'center',justifyContent: 'center',}}>
+        <Text>SAMPLE</Text></View>
+      <View style ={{flex: 0.05,flexDirection: 'row',marginLeft: 60}}>
+      <Text>Username: </Text>
+      <View style ={{borderRadius:30,backgroundColor: 'white',flex:0.8,marginVertical:0}}><TextInput></TextInput></View>
+      </View>
+      <View style ={{flex: 0.1,flexDirection: 'row',marginLeft: 60}}>
+      <Text>Password:</Text>
+      <View><TextInput></TextInput></View>
+      </View>
+      <View style ={{flex: 0.05 ,alignItems: 'center',justifyContent: 'center',}}>
+        <Text>LOGIN</Text></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'yellow',
+    
   },
 });
